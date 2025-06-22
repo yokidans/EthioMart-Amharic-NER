5b6be6279c3a9f27e4ef44b5c66da73b179fb783
Teferi Mamo <teferi6245@gmail.com>
2025-06-22 21:40:51 +0300

    Update run_pipeline.py

--- src/run_pipeline.py
+++ src/run_pipeline.py
@@ -3,6 +3,7 @@ from src.data_ingestion.telegram_scraper import EthioMartScraper
 from src.preprocessing.data_cleaner import EthioMartPreprocessor
 from src.modeling.train_ner import train
 
+
 def full_pipeline():
     # Step 1: Data collection
     scraper = EthioMartScraper()
@@ -17,6 +18,7 @@ def full_pipeline():
     train()
     
     print("Pipeline executed successfully")
+
 
 if __name__ == "__main__":
     full_pipeline()
